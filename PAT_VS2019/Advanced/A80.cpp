29159d1d87966b3484e4bbad1fb52e952ca8b782
shiwanghua <2804324275@qq.com>
2020-01-09 21:02:06 +0800

    2020-01-09

--- PAT_VS2019/Advanced/A80.cpp
+++ PAT_VS2019/Advanced/A80.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
-#include<string>
-#include<memory.h>
-#include <iomanip>
-#include <vector>
-#include<algorithm>
-#include<math.h>
-
-#define _for(a,b) for( int i=a; i<b; ++i)
-#define __for(a,b) for(int i=a; i<=b;++i)
-#define mfor(a,b) for(int i=a;i>=b;--i)
-
-using namespace std;
-
-int main()
-{
-
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include<memory.h>
+//#include <iomanip>
+//#include <vector>
+//#include<algorithm>
+//#include<math.h>
+//
+//#define _for(a,b) for( int i=a; i<b; ++i)
+//#define __for(a,b) for(int i=a; i<=b;++i)
+//#define mfor(a,b) for(int i=a;i>=b;--i)
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n, m, k;
+//	cin >> n >> m >> k;
+//	int* q = new int[m+1];
+//	_for(1, m + 1)cin >> q[i];
+//
+//	return 0;
+//}
--- PAT_VS2019/Advanced/A95.cpp
+++ PAT_VS2019/Advanced/A95.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
-#include<string>
-#include <iomanip>
-#include <vector>
-#include<algorithm>
-
-#define _for(a,b) for( int i=a; i<b; ++i)
-using namespace std;
-
-int main()
-{
-	int n;
-
-
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include <iomanip>
+//#include <vector>
+//#include<algorithm>
+//
+//#define _for(a,b) for( int i=a; i<b; ++i)
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//
+//
+//	return 0;
+//}
