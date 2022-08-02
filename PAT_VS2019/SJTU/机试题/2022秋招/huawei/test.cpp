d1e544e35490abc3cf608e95fd3adf324d1f77d2
Swhua <2804324275@qq.com>
2022-08-02 17:27:17 +0800

    Add files via upload

---
+++ PAT_VS2019/SJTU/机试题/2022秋招/huawei/test.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    string paragraph;
+    unordered_map<string, int> wordCount;
+    unordered_set<string> banned;
+    banned.insert("hit");
+    // cin >> paragraph;
+    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+
+    int n = paragraph.length();
+    int maxCount = 0;
+    string maxCountString;
+
+    for (int i = 0; i < n;)
+    {
+        int j = i;
+        string word = "";
+        while (j < n && isalpha(paragraph[j]))
+        {
+            // char c = (paragraph[j]>='A' && paragraph[j]<='Z') ? paragraph[j]+32:paragraph[j];
+            // cout<<paragraph[j]<<", "<<c<<"\n";
+            word.append(string(1,tolower(paragraph[j])));
+            j++;
+        }
+
+        wordCount[word]++;
+
+        while (j < n && !isalpha(paragraph[j]))
+            j++;
+        i = j;
+    }
+
+    for (auto &[word, cnt] : wordCount)
+    {
+        if (cnt > maxCount&&banned.count(word)==0)
+        {
+            maxCount = cnt;
+            maxCountString = word;
+        }
+    }
+
+    cout<<maxCountString<<"\n";
+
+    return 0;
+}
